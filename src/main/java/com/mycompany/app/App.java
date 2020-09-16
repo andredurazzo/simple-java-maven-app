ff15740f805f7efeb2ff2a34bb31b48fbc9fec6e
andre.durazzo <andre.durazzo@amaro.com>
2020-09-16 19:06:39 -0300

    if error send email

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,24 +1,21 @@
 package com.mycompany.app;
 
-import java.util.Optional;
-
 /**
  * Hello world!
  */
 public class App
 {
 
-    private final String message = "Hello World!";
-
-    public App() {}
+  public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
 
-    private final String getMessage() {
+    private String getMessage() {
       var text = " Java 11";
-        return message.concat(text);
+      final String message = "Hello World!";
+      return message.concat(text);
     }
 
 }
